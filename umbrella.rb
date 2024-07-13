d7d1d02730956fce71ac2cf1a0a4c39f9186c8c8
Daniela Garcia <danielagarcia13.99@gmail.com>
2024-07-13 05:24:10 +0000

    fixed link error (missing '&' before key)

--- umbrella.rb
+++ umbrella.rb
@@ -10,7 +10,5 @@ location = gets.chomp
 
 #API call to fetch location latitude and longitude information
 access_gmaps_key = ENV.fetch("GMAPS_KEY")
-gmaps_location= HTTP.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{location}key=#{access_gmaps_key}")
+gmaps_location= HTTP.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{location}&key=#{access_gmaps_key}")
 puts gmaps_location
-
-
